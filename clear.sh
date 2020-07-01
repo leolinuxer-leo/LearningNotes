eded8a0f52277927a765b3bce04133620d4429a2
yinzhao <yinzhao@in-MacBook-Pro.local>
2020-07-01 13:04:53 +0800

    add 从一元一次方程到伽罗瓦理论/2.群论的预备知识

---
+++ clear.sh
@@ -0,0 +1,6 @@
+find . -name '*.aux' | xargs rm 
+find . -name '*.bbl' | xargs rm
+find . -name '*.blg' | xargs rm
+find . -name '*.log' | xargs rm
+find . -name '*.gz' | xargs rm
+find . -name '*.toc' | xargs rm
