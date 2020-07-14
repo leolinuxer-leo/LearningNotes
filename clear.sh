e59e7bcabf9e3150a0882f4d8368aed3674d128c
yinzhao <yinzhao@in-MacBook-Pro.local>
2020-07-14 14:57:39 +0800

    add 技术资料

--- clear.sh
+++ clear.sh
@@ -1,7 +1,7 @@
-find . -name '*.aux' | xargs rm 
-find . -name '*.bbl' | xargs rm
-find . -name '*.blg' | xargs rm
-find . -name '*.log' | xargs rm
-find . -name '*.gz' | xargs rm
-find . -name '*.toc' | xargs rm
-find . -name '*.fls' | xargs rm
+find . -name '*.aux'  -print0 | xargs -0 rm 
+find . -name '*.bbl'  -print0 | xargs -0 rm
+find . -name '*.blg'  -print0 | xargs -0 rm
+find . -name '*.log' -print0  | xargs -0 rm
+find . -name '*.gz'  -print0 | xargs -0 rm
+find . -name '*.toc' -print0  | xargs -0 rm
+find . -name '*.fls'  -print0 | xargs -0 rm
